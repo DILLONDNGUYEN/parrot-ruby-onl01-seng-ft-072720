2f666affd5969ecb515fdc8cd0cc305d55124478
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-27 20:33:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(speak="Squawk!")
+def parrot(speak="Squawk!", bird="Pretty Bird!")
   puts "#{speak}"
+  puts "#{bird}"
   return "Squawk!"
 end
